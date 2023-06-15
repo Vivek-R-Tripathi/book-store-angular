65dd9cc431b61a7359814ff05fce2b8ae04f1463
vivek <sanguine.vivek@gmail.com>
2023-06-15 18:21:29 +0530

    some operation related to the @viewchild

--- BookSApp/src/app/public-component/home/home.component.ts
+++ BookSApp/src/app/public-component/home/home.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { AuthComponent } from 'src/app/auth/auth/auth.component';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 import { AuthorsComponent } from 'src/app/shared/components/authors/authors.component';
 
 @Component({
@@ -13,13 +13,25 @@ export class HomeComponent  implements AfterViewInit{
   
   @ViewChild(AuthorsComponent)
   private childAuthur!: AuthorsComponent;
+
+  //Using ths @viewchild we make changes on the mat-material properties as well
+  @ViewChild('counterBtn')
+  private counterBtn!: MatButton;
   name1:string="X-man";
+
+  // we can use this to update some property related to the native element of the html
+  @ViewChild('title')
+  private title !: ElementRef;
 
   ngAfterViewInit(): void {
   setTimeout(() => {
     this.childAuthur.setData(2);
     this.childAuthur.fullNames("Vivek Ram Tripathi");
     this.childAuthur.name1 =this.name1;
+    this.counterBtn.color = 'primary';
+    this.counterBtn.disabled = true;
+    // we can console this native element and can use according to the requirement
+    this.title.nativeElement.innerHTML ='This is my updated Title';
   }, 0);
    
   }
