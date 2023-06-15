cc788eb37c2e05dc374f8869dd80956e11d66598
vivek <sanguine.vivek@gmail.com>
2023-06-15 17:50:20 +0530

    Implementation of @ViewChild

--- BookSApp/src/app/public-component/home/home.component.ts
+++ BookSApp/src/app/public-component/home/home.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AuthComponent } from 'src/app/auth/auth/auth.component';
+import { AuthorsComponent } from 'src/app/shared/components/authors/authors.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent  implements AfterViewInit{
+ 
+  fullName:string='';
+  
+  @ViewChild(AuthorsComponent)
+  private childAuthur!: AuthorsComponent;
+  name1:string="X-man";
 
-
-  name:string="vivek";
+  ngAfterViewInit(): void {
+  setTimeout(() => {
+    this.childAuthur.setData(2);
+    this.childAuthur.fullNames("Vivek Ram Tripathi");
+    this.childAuthur.name1 =this.name1;
+  }, 0);
+   
+  }
 }
--- BookSApp/src/app/shared/components/authors/authors.component.ts
+++ BookSApp/src/app/shared/components/authors/authors.component.ts
@@ -9,9 +9,21 @@ export class AuthorsComponent {
 
   public data: number | undefined
 
+
+
+  public fullName !: string;
+
+  public name1 !: string;
+
   public setData(value:number):void{
    this.data=value;
   }
+
+  public fullNames(value:string):void{
+     this.fullName=value
+   }
+
+  
 
   // @Input() data: string | undefined;
 }
