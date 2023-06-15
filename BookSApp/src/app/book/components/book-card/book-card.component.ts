0e6bb8d043ab6cb118cf3c554a25d6c7964525e9
vivek <sanguine.vivek@gmail.com>
2023-06-15 17:00:44 +0530

    Getter setter implementation!!

--- BookSApp/src/app/book/components/book-card/book-card.component.ts
+++ BookSApp/src/app/book/components/book-card/book-card.component.ts
@@ -8,6 +8,20 @@ import { BookModel } from '../../BookModel/book.Model';
 })
 export class BookCardComponent {
 
+//Getter and setting concept is used for updation oject values through out application!!
+//If we want to used the certain data to be added throughly in applicaion then we can use concept of getter setter!!
  @Input()
-  books!: BookModel[];
+  //books!: BookModel[];
+
+  get books(): BookModel[]{
+    return this._books
+  }
+
+  set books(books: BookModel[])
+  {
+    books.map(x=>x.title = 'Title:' +x.title)
+    this._books = books;
+  }
+
+  private _books!: BookModel[];
 }
