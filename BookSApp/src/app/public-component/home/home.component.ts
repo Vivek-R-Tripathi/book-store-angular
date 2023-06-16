923b53689ee909987a8124f366a226cc95683a1c
vivek <sanguine.vivek@gmail.com>
2023-06-16 23:59:49 +0530

    Oninit& Docheck implementation

--- BookSApp/src/app/public-component/home/home.component.ts
+++ BookSApp/src/app/public-component/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnChanges, OnInit} from '@angular/core';
+import { AuthorModel } from 'src/app/shared/components/AuthorModel/Author.model';
 
 @Component({
   selector: 'app-home',
@@ -14,13 +15,17 @@ export class HomeComponent implements OnInit{
   ngOnInit(): void {
     console.log("This is inside Parent NG-OnInit");
   }
-
+  
+  public authModel:AuthorModel = {AuthorId:23,AuthorName:"Vivek Ram Tripathi"}
   count:number = 0;
 
   // When there is change in count i.e, the input property the onchange method got execute with 3 values current, previous 
   public incounter():void {
-
     this.count++;
+    //we r up updating existing reference of an object the ng on changes will not detect this changes 
+    this.authModel.AuthorId = 34;
+    //Now in this sceanrio we are again newly instanciate the AuthModel so it ngOnChnage will detect their value
+   // this.authModel ={AuthorId:667,AuthorName:"Lalu"}
   }
 
 }
--- BookSApp/src/app/shared/components/authors/authors.component.ts
+++ BookSApp/src/app/shared/components/authors/authors.component.ts
@@ -1,11 +1,12 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, DoCheck, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { AuthorModel } from '../AuthorModel/Author.model';
 
 @Component({
   selector: 'app-authors',
   templateUrl: './authors.component.html',
   styleUrls: ['./authors.component.scss']
 })
-export class AuthorsComponent implements OnChanges, OnInit {
+export class AuthorsComponent implements OnChanges, OnInit, DoCheck {
  
   /**
    *
@@ -14,10 +15,17 @@ export class AuthorsComponent implements OnChanges, OnInit {
     console.log("This is inside Child Constructor");
     
   }
+  ngDoCheck(): void {
+    // Here it will detect the changes from Updated value with same referece from CounterMethod in Parent
+    console.log(this.authModel);
+  }
   ngOnInit(): void {
     console.log("This is inside Child NG-OnInit");
   }
   @Input() count ! :number;
+
+  @Input()
+  authModel!: AuthorModel;
 
   ngOnChanges(changes: SimpleChanges): void {
 
