93a0ad5225f259c762ad0873002e158261b00943
vivek <sanguine.vivek@gmail.com>
2023-06-20 18:27:21 +0530

    Validation and default value

--- BookSApp/src/app/book/components/add-book-reactive/add-book-reactive.component.ts
+++ BookSApp/src/app/book/components/add-book-reactive/add-book-reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { BookModel } from '../../BookModel/book.Model';
 import { BookService } from '../../services/book.service';
 
@@ -37,7 +37,7 @@ export class AddBookReactiveComponent implements OnInit {
 
   private formInit():void{
     this.formObj = new FormGroup({
-      title:new FormControl(),
+      title:new FormControl('', [Validators.required,Validators.minLength(10)]),
       author: new FormControl(),
       pages: new FormControl(),
       language: new FormControl(),
